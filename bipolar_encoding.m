530f25c1360cb33910ea57491d066aba7a2f6d67
demetbayraktar <87068783+demetbayraktar@users.noreply.github.com>
2021-07-09 10:32:28 +0300

    version3

--- bipolar_encoding.m
+++ bipolar_encoding.m
@@ -1,7 +1,18 @@
-n=8;
+n=8; %stream size 
+
+%requests number of ones from the user
 prompt = 'Enter ';
-Y = input(prompt)
-g = round((Y+n)/2)
+
+%getting the number of ones from the user
+Y = input(prompt) 
+
+g = round((Y+n)/2) %BPU formula
+
+%put all zeros inside the stream
 Z = zeros(1,n);
+
+%puts the input we get from the user
 Z(1:g)=1;
+
+%n-bit stochastic number by BPU
 Z = Z(randperm(numel(Z)))
