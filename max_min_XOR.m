3fb883675a209d037d97fb89496848ebe2973d51
demetbayraktar <87068783+demetbayraktar@users.noreply.github.com>
2021-07-26 21:20:48 +0300

    last version

--- max_min_XOR.m
+++ max_min_XOR.m
@@ -9,13 +9,7 @@ N = 8; %define the stream size
 
 a_max = min(X1,X2);%maxiumum pair of 11's
 b_min = X1-a_max; %minimum pair of 10's
-c_min = X2-a_max; %minimum pair of 01's
-d_max = min((N-X1),(N-X2)); %maximum pair of 00's
-a_min = max(0,X1+X2-N); %minimum pair of 11's
-b_max = X1-a_min; %maxiumum pair of 10's
-c_max = X2-a_min; %maximum pair of 01's
-d_min = max(0,N-X1+X2); %minimum pair of 00's
- y = xor(X1,X2)
+y = xor(X1,X2)
 %getting input from user
 variable = input('If you want to see maximum correlation type 1\nIf you want to see minimum correlation type 2\nType: ');
 
@@ -27,6 +21,7 @@ for y = xor(X1,X2)
         disp(a_min);
     end
 end
+
 
 
         
